871b937516d3a6ca8d7a03dfdd246d6c2a2fecba
Miriam Wangui <wanguikings@gmail.com>
2023-04-24 08:59:45 -0700

    Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char c = 'a';
-
-	while (c <= 'z')
 {
-	if (c != 'e' &&  c != 'q')
-
-	putchar (c);
+	char c = 'a';
+	
+	while (c <= 'z)
+	{
+	    if (c != 'e' && c != 'q')
+	{
+		putchar(c);
+	}
 	c++;
+{
+    putchar('\n');
+    return (0);
 }
-	putchar ('\n');
-	return (0);
-}
+
