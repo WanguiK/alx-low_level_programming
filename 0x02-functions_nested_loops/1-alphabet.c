5addbc7894297f76b9018e4eda6a4212a5d5c50f
Miriam Wangui <wanguikings@gmail.com>
2023-04-25 07:10:27 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- * main- prints the alphabet in lowercase
+ * print_alphabet- prints the alphabet in lowercase
+ *
+ * Description: This function prints the lowercase alphabet using the _putchar
+ *              function. It starts with 'a' and ends with 'z', followed by a
+ *              newline character
  *
  * Return: Always 0
  */
