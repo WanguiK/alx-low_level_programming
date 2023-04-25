dc1044051369b15d1b0aaf71e8d9bf39fca0850e
Miriam Wangui <wanguikings@gmail.com>
2023-04-25 12:10:24 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add- adds two integers and returns the result
+ *@a: The character to print
+ *@b: The character to print
+ *
+ *Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
