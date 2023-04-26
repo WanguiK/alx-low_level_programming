5c52386d5bd5baae412a6ce5920ee4617428c327
Miriam Wangui <wanguikings@gmail.com>
2023-04-26 02:24:06 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,18 +11,18 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (b > c && c > a)
+
+	if (a > b && a > c)
+	{
+	largest = a;
+	}
+	else if (b > a && b > c)
 	{
 	largest = b;
 	}
-	else if (c > b && b > a)
-	{
-	largest = c;
-	}
 	else
 	{
-	largest = a;
+	largest = c;
 	}
 
 	return (largest);
