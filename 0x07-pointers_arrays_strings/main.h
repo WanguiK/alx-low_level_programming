992729b04b0197396375cb1fb6c629d3b437b631
Miriam Wangui <wanguikings@gmail.com>
2023-05-08 01:28:00 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 
-int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
