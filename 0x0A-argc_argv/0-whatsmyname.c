20206ec283e0a82608c4ed6b7b02e456ef20280c
Miriam Wangui <wanguikings@gmail.com>
2023-05-12 04:43:39 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main- prints new name followed by new line
+ *
+ * Return: 0 Always
+ */
+int main (int __attribute__((unused)) argc, char *argv[])
+{
+	printf ("%s\n", argv[0]);
+	return (0);
+}
