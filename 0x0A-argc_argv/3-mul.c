4d38745733286facf0326faa1b7c700150e7a856
Miriam Wangui <wanguikings@gmail.com>
2023-05-12 06:46:07 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main- multiplies two numbers
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: 0 in success and 1 in Error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+	
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		product = num1 * num2;
+
+		printf("%d\n", product);
+	}
+	else if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
