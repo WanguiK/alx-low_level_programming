31d73ea78cd7213277eb3e7e26d6cfbce7a1ad8f
Miriam Wangui <wanguikings@gmail.com>
2023-05-22 04:29:51 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,15 +12,15 @@
  * Return: 0 Always
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-#endif /* MAIN_H */
+#endif /* DOG_H */
