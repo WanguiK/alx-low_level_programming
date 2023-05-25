fa555d0e3a09fdb1117a236f9535a046d1bcaede
Miriam Wangui <wanguikings@gmail.com>
2023-05-25 05:39:18 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,16 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d ", va_arg(ap, int));
-		if (separator != NULL && i < n - 1)
+		printf("%d",  va_arg(ap, int));
+		if (i != (n-1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-		else
-		{	
-			return;
-		}	
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
