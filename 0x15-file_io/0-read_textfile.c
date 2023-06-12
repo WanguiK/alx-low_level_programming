c055f28c9ad2950a2a85dd7ba027d5d268b4673f
WanguiK <wanguikings@gmail.com>
2023-06-12 11:46:58 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,21 +11,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *s;
-	FILE *fp;
+	char *buf;
+	ssize_t fd;
 	ssize_t w;
-	ssize_t r;
+	ssize_t t;
 
-	fp = fopen(filename, "r");
-	if (fd == NULL)
-	{
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	}
-	s = malloc(sizeof(char) * letters);
-	r = read(fp, s, letters);
-	w = write(STDOUT_FILENO, s, r);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	free(s);
-	close(fp);
+	free(buf);
+	close(fd);
 	return (w);
 }
