5a7cb11ed15f57b1d8dfc1e614057fd1af4b972a
WanguiK <wanguikings@gmail.com>
2023-08-17 07:53:25 -0700

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,52 +3,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index- inserts a new node at a given position
+ * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: head pointer of the double linked list
+ * @idx: index of the list where the new node should be added
  * @n: value to be stored in the new node
- * @idx:  index of the list where the new node should be added
  * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode = add_dnodeint(n);
+	unsigned int i = 0;
+	dlistint_t *current = *h;
 
-	if (*h == NULL)
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
 	{
-		if (idx != 0)
-		{
-			free(newNode);
-			return (NULL);
-		}
-		*h = newNode;
-		return (newNode);
+		return (NULL);
 	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 	if (idx == 0)
 	{
-		newNode->next = *h;
-		(*h)->prev = newNode;
-		*h = newNode;
-		return (newNode);
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
 	}
-	dlistint_t *current = *h;
-	unsigned int i = 0;
-
-	while (current != NULL && i < idx)
+	while (current != NULL)
 	{
+		if (i == idx - 1)
+		{
+			new_node->prev = current;
+			new_node->next = current->next;
+			if (current->next != NULL)
+			{
+				current->next->prev = new_node;
+			}
+			current->next = new_node;
+			return (new_node);
+		}
 		current = current->next;
 		i++;
 	}
-	if (current == NULL && i != idx)
-	{
-		free(newNode);
-		return (NULL);
-	}
-	newNode->prev = current->prev;
-	newNode->next = current;
-	if (current->prev != NULL)
-	{
-		current->prev->next = newNode;
-	}
-	current->prev = newNode;
-	return (newNode);
+	free(new_node);
+	return (NULL);
 }
