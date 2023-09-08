a1392d84dd69e1908bed7df32e202799a2ce663c
WanguiK <wanguikings@gmail.com>
2023-09-08 06:08:53 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned int i = 0;
 
 	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -17,5 +18,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
